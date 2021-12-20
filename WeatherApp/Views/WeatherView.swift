6f7c7aefd60ee75d93aa11494ab376fcd5ac0c9a
hari13haran <51775304+hari13haran@users.noreply.github.com>
2021-12-20 12:56:21 +0530

    Basic weather View design with temperature & description + image

--- WeatherApp/Views/WeatherView.swift
+++ WeatherApp/Views/WeatherView.swift
@@ -11,9 +11,57 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(weather.name)
-            .bold()
-            .font(.title)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name).bold().font(.title)
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "smoke")
+                                .font(.system(size: 40))
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 80))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) {
+                        image in
+                        image.resizable()
+                             .aspectRatio(contentMode: .fit)
+                             .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.619, saturation: 0.894, brightness: 0.379))
+        .preferredColorScheme(.dark)
         
     }
 }
